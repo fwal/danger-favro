452d8af4937a6b0ee1bfa6eb1c43163e349d538b
Frederik Wallner <frederik.wallner@gmail.com>
2017-10-26 00:34:34 +0200

    Favro api client

---
+++ lib/favro/api_client.rb
@@ -0,0 +1,29 @@
+require 'rest-client'
+
+module Danger
+  class ApiClient
+    def initialize(user_name, api_token, organization_id)
+      @user_name = user_name
+      @api_token = api_token
+      @organization_id = organization_id
+    end
+
+    def get(ticket_ids)
+      tickets = []
+      ticket_ids.each do |id|
+        response = request(id)
+        tickets << Ticket.new(id, response.nil? ? nil : response["entities"][0]["name"])
+      end
+      tickets
+    end
+
+    private
+
+    def request(ticket_id)
+      response = RestClient::Request.execute method: :get, url: "https://favro.com/api/v1/cards?cardSequentialId=#{ticket_id}&unique=true&limit=1", headers: { accept: :json, organizationId: @organization_id }, user: @user_name, password: @api_token
+      JSON.parse(response)
+    rescue RestClient::ExceptionWithResponse
+      nil
+    end
+  end
+end
